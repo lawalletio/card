77c39503ad2eae3a58998156ead3ddea2df5c4bc
Juan Ramírez <artjjrn@gmail.com>
2023-11-22 19:25:21 -0300

    Add publish data endpoint

---
+++ src/rest/card/publish-data/post.ts
@@ -0,0 +1,37 @@
+import { Debugger } from 'debug';
+import { Response } from 'express';
+
+import { buildCardDataEvent } from '@lib/config';
+import { getTagValue, parseEventBody } from '@lib/event';
+import { logger } from '@lib/utils';
+import { ExtendedRequest } from '@type/request';
+import { NostrEvent } from '@nostr-dev-kit/ndk';
+
+const log: Debugger = logger.extend('rest:card:publish-data:post');
+const error: Debugger = log.extend('error');
+
+/**
+ * Triggers a card data event publication to nostr
+ */
+const handler = async (req: ExtendedRequest, res: Response) => {
+  const reqEvent: NostrEvent | null = parseEventBody(req.body);
+  if (!reqEvent) {
+    res.status(422).send();
+    return;
+  }
+  if ('card-publish-data' !== getTagValue(reqEvent, 't')) {
+    log('Received incorrect request');
+    res.status(422).send();
+    return;
+  }
+  try {
+    const event = await buildCardDataEvent(reqEvent.pubkey);
+    await req.context.outbox.publish(event);
+    res.status(200).send();
+  } catch (e) {
+    error('Unexpected error: %O', e);
+    res.status(500).send();
+  }
+};
+
+export default handler;
---
+++ tests/rest/card/publish-data/post.test.ts
@@ -0,0 +1,114 @@
+import { buildCardDataEvent } from '@lib/config';
+import { getTagValue, parseEventBody } from '@lib/event';
+import { NostrEvent } from '@nostr-dev-kit/ndk';
+
+import handler from '@rest/card/publish-data/post';
+
+jest.mock('@lib/config', () => {
+  return {
+    __esModule: true,
+    buildCardDataEvent: jest.fn(),
+  };
+});
+jest.mock('@lib/event', () => {
+  return {
+    __esModule: true,
+    getTagValue: jest.fn(),
+    parseEventBody: jest.fn(),
+  };
+});
+
+describe('POST to /card/publish-data', () => {
+  it('should publish event when received valid request', async () => {
+    const pubkey =
+      '9e34efffcc194e9636392a5937ce7986aef62f5f36b62312dcc7ddecd606b175';
+    const reqEvent = { pubkey };
+    const dataEvent = {};
+    jest.mocked(parseEventBody).mockReturnValue(reqEvent as NostrEvent);
+    jest.mocked(getTagValue).mockReturnValue('card-publish-data');
+    jest.mocked(buildCardDataEvent).mockResolvedValue(dataEvent as NostrEvent);
+    const body = {};
+    const req: any = {
+      context: {
+        outbox: {
+          publish: jest.fn(),
+        },
+      },
+      body,
+    };
+    const res: any = {
+      status: jest.fn().mockReturnValue({ send: jest.fn() }),
+    };
+
+    await handler(req, res);
+
+    expect(buildCardDataEvent).toHaveBeenCalledWith(pubkey);
+    expect(req.context.outbox.publish).toHaveBeenCalledWith(dataEvent);
+    expect(res.status).toHaveBeenCalledWith(200);
+  });
+
+  it('should fail when received invalid request', async () => {
+    jest.mocked(parseEventBody).mockReturnValue(null);
+    const req: any = {
+      context: {
+        outbox: {
+          publish: jest.fn(),
+        },
+      },
+    };
+    const res: any = {
+      status: jest.fn().mockReturnValue({ send: jest.fn() }),
+    };
+
+    await handler(req, res);
+
+    expect(res.status).toHaveBeenCalledWith(422);
+  });
+
+  it('should fail when received invalid request tag', async () => {
+    jest.mocked(parseEventBody).mockReturnValue({} as NostrEvent);
+    jest.mocked(getTagValue).mockReturnValue('invalid-tag');
+    const req: any = {
+      context: {
+        outbox: {
+          publish: jest.fn(),
+        },
+      },
+    };
+    const res: any = {
+      status: jest.fn().mockReturnValue({ send: jest.fn() }),
+    };
+
+    await handler(req, res);
+
+    expect(res.status).toHaveBeenCalledWith(422);
+  });
+
+  it('should fail when publishing throws error', async () => {
+    const pubkey =
+      '9e34efffcc194e9636392a5937ce7986aef62f5f36b62312dcc7ddecd606b175';
+    const reqEvent = { pubkey };
+    const dataEvent = {};
+    jest.mocked(parseEventBody).mockReturnValue(reqEvent as NostrEvent);
+    jest.mocked(getTagValue).mockReturnValue('card-publish-data');
+    jest.mocked(buildCardDataEvent).mockResolvedValue(dataEvent as NostrEvent);
+    const body = {};
+    const req: any = {
+      context: {
+        outbox: {
+          publish: jest.fn().mockRejectedValue(''),
+        },
+      },
+      body,
+    };
+    const res: any = {
+      status: jest.fn().mockReturnValue({ send: jest.fn() }),
+    };
+
+    await handler(req, res);
+
+    expect(buildCardDataEvent).toHaveBeenCalledWith(pubkey);
+    expect(req.context.outbox.publish).toHaveBeenCalledWith(dataEvent);
+    expect(res.status).toHaveBeenCalledWith(500);
+  });
+});
