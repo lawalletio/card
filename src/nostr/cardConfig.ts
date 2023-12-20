a40e324e615d6b02e1a525a0358e3f9e4ec05ea5
Mariano Pérez Rodríguez <mariano.perez.rodriguez@gmail.com>
2023-12-20 20:46:53 -0300

    Add event catch

--- src/nostr/cardConfig.ts
+++ src/nostr/cardConfig.ts
@@ -1,7 +1,9 @@
 import type { NDKFilter, NostrEvent } from '@nostr-dev-kit/ndk';
 
+import { Debugger } from 'debug';
+
 import { Kind, buildMultiNip04Event } from '@lib/event';
-import { nowInSeconds, requiredEnvVar } from '@lib/utils';
+import { logger, nowInSeconds, requiredEnvVar } from '@lib/utils';
 
 import {
   ConfigTypes,
@@ -26,6 +28,9 @@ const filter: NDKFilter = {
 const cardPrivateKey: string = requiredEnvVar('NOSTR_PRIVATE_KEY');
 const cardPublicKey: string = requiredEnvVar('NOSTR_PUBLIC_KEY');
 
+const log: Debugger = logger.extend('rest:card:data:request:post');
+const error: Debugger = log.extend('error');
+
 /**
  * Return the internal-transaction-ok handler
  */
@@ -42,114 +47,118 @@ const getHandler = (ctx: Context): ((event: NostrEvent) => void) => {
       cardPublicKey,
     );
 
-    await ctx.prisma.$transaction(async (tx) => {
-      if ('trusted-merchants' in content) {
-        const merchantPubKeys: string[] = (
-          await tx.merchant.findMany({
+    try {
+      await ctx.prisma.$transaction(async (tx) => {
+        if ('trusted-merchants' in content) {
+          const merchantPubKeys: string[] = (
+            await tx.merchant.findMany({
+              select: {
+                pubKey: true,
+              },
+            })
+          ).map((merchant: { pubKey: string }): string => merchant.pubKey);
+          await tx.trustedMerchants.deleteMany({
+            where: {
+              holderPubKey: holderPubKey,
+            },
+          });
+          await tx.trustedMerchants.createMany({
+            data: content['trusted-merchants']
+              .filter((trustedMerchant: { pubkey: string }): boolean =>
+                merchantPubKeys.includes(trustedMerchant.pubkey),
+              )
+              .map(
+                (trustedMerchant: {
+                  pubkey: string;
+                }): Prisma.TrustedMerchantsCreateManyInput => {
+                  return {
+                    holderPubKey: holderPubKey,
+                    merchantPubKey: trustedMerchant.pubkey,
+                  };
+                },
+              ),
+          });
+        }
+
+        const holderCardUuids: string[] = (
+          await tx.card.findMany({
+            where: {
+              holderPubKey: holderPubKey,
+            },
             select: {
-              pubKey: true,
+              uuid: true,
             },
           })
-        ).map((merchant: { pubKey: string }): string => merchant.pubKey);
-        await tx.trustedMerchants.deleteMany({
-          where: {
-            holderPubKey: holderPubKey,
-          },
-        });
-        await tx.trustedMerchants.createMany({
-          data: content['trusted-merchants']
-            .filter((trustedMerchant: { pubkey: string }): boolean =>
-              merchantPubKeys.includes(trustedMerchant.pubkey),
-            )
-            .map(
-              (trustedMerchant: {
-                pubkey: string;
-              }): Prisma.TrustedMerchantsCreateManyInput => {
-                return {
-                  holderPubKey: holderPubKey,
-                  merchantPubKey: trustedMerchant.pubkey,
-                };
-              },
-            ),
-        });
-      }
+        ).map((x: { uuid: string }) => x.uuid);
 
-      const holderCardUuids: string[] = (
-        await tx.card.findMany({
-          where: {
-            holderPubKey: holderPubKey,
-          },
-          select: {
-            uuid: true,
-          },
-        })
-      ).map((x: { uuid: string }) => x.uuid);
+        const modifications: Promise<unknown>[] = [];
 
-      const modifications: Promise<unknown>[] = [];
+        for (const uuid in content.cards) {
+          if (!holderCardUuids.includes(uuid)) {
+            continue;
+          }
+          if ('limits' in content.cards[uuid]) {
+            modifications.push(
+              (async function (cardUuid: string) {
+                await tx.limit.deleteMany({ where: { cardUuid } });
+                await tx.limit.createMany({
+                  data: content.cards[uuid].limits.map(
+                    (limit: Limit): Prisma.LimitCreateManyInput => {
+                      return {
+                        cardUuid,
+                        amount: limit.amount,
+                        delta: limit.delta,
+                        description: limit.description,
+                        name: limit.name,
+                        token: limit.token,
+                      };
+                    },
+                  ),
+                });
+              })(uuid),
+            );
+          }
 
-      for (const uuid in content.cards) {
-        if (!holderCardUuids.includes(uuid)) {
-          continue;
-        }
-        if ('limits' in content.cards[uuid]) {
+          const cardUpdate: Prisma.CardUpdateInput = {};
+          if ('name' in content.cards[uuid]) {
+            cardUpdate.name = content.cards[uuid].name;
+          }
+          if ('description' in content.cards[uuid]) {
+            cardUpdate.description = content.cards[uuid].description;
+          }
+          if ('status' in content.cards[uuid]) {
+            cardUpdate.enabled =
+              content.cards[uuid].status === CardStatus.ENABLED;
+          }
+
           modifications.push(
-            (async function (cardUuid: string) {
-              await tx.limit.deleteMany({ where: { cardUuid } });
-              await tx.limit.createMany({
-                data: content.cards[uuid].limits.map(
-                  (limit: Limit): Prisma.LimitCreateManyInput => {
-                    return {
-                      cardUuid,
-                      amount: limit.amount,
-                      delta: limit.delta,
-                      description: limit.description,
-                      name: limit.name,
-                      token: limit.token,
-                    };
-                  },
-                ),
-              });
-            })(uuid),
+            tx.card.update({ where: { uuid }, data: cardUpdate }),
           );
         }
 
-        const cardUpdate: Prisma.CardUpdateInput = {};
-        if ('name' in content.cards[uuid]) {
-          cardUpdate.name = content.cards[uuid].name;
-        }
-        if ('description' in content.cards[uuid]) {
-          cardUpdate.description = content.cards[uuid].description;
-        }
-        if ('status' in content.cards[uuid]) {
-          cardUpdate.enabled =
-            content.cards[uuid].status === CardStatus.ENABLED;
-        }
+        await Promise.all(modifications);
 
-        modifications.push(
-          tx.card.update({ where: { uuid }, data: cardUpdate }),
+        const configAckEvent: NostrEvent = await buildMultiNip04Event(
+          JSON.stringify(
+            await buildCardConfigPayload(holderPubKey, tx as PrismaClient),
+            (_, v) => (typeof v === 'bigint' ? String(v) : v),
+          ),
+          cardPrivateKey,
+          cardPublicKey,
+          [cardPublicKey, holderPubKey],
         );
-      }
+        configAckEvent.kind = Kind.PARAMETRIZED_REPLACEABLE.valueOf();
+        configAckEvent.tags.concat([
+          ['e', event.id!],
+          ['t', ConfigTypes.CONFIG.valueOf()],
+          ['d', `${holderPubKey}:${ConfigTypes.CONFIG.valueOf()}`],
+        ]);
 
-      await Promise.all(modifications);
-
-      const configAckEvent: NostrEvent = await buildMultiNip04Event(
-        JSON.stringify(
-          await buildCardConfigPayload(holderPubKey, tx as PrismaClient),
-          (_, v) => (typeof v === 'bigint' ? String(v) : v),
-        ),
-        cardPrivateKey,
-        cardPublicKey,
-        [cardPublicKey, holderPubKey],
-      );
-      configAckEvent.kind = Kind.PARAMETRIZED_REPLACEABLE.valueOf();
-      configAckEvent.tags.concat([
-        ['e', event.id!],
-        ['t', ConfigTypes.CONFIG.valueOf()],
-        ['d', `${holderPubKey}:${ConfigTypes.CONFIG.valueOf()}`],
-      ]);
-
-      await ctx.outbox.publish(configAckEvent);
-    });
+        await ctx.outbox.publish(configAckEvent);
+      });
+    } catch (e) {
+      error('Unexpected error: %O', e);
+    }
   };
 };
 
